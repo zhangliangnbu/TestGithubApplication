5d1bb51e963a0cb845b7523eb9f9aaa912fd7010
zhangliang <zhangliangnbu@163.com>
2017-09-07 14:58:54 +0800

    add message about how to connetc a new project connect github using android studio

--- app/src/main/java/com/liang/testgithubapplication/MainActivity.java
+++ app/src/main/java/com/liang/testgithubapplication/MainActivity.java
@@ -3,6 +3,11 @@ package com.liang.testgithubapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 第一步：检查、安装git；注册GitHub
+ * 第二步：创建项目并使用git初始化，VCS->Enable Version Control Integration...；(可编辑.gitignore文件)
+ * 第三步：与GitHub建立远程连接，VCS->Share Project on Github
+ */
 public class MainActivity extends AppCompatActivity {
 
 	@Override
